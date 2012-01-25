81b4bd891d4dc9e508df7f8ec9287cc598de7c7f
Frank LENORMAND <lenormf@gmail.com>
2012-01-25 22:35:46 +0100

    [SRC] Added more functions to the package, factorized the code

--- notify/notify.go
+++ notify/notify.go
@@ -33,7 +33,8 @@ const (
 	NOTIFY_URGENCY_CRITICAL = 2;
 )
 
-type NotifyUrgency int;
+type NotifyUrgency int
+type NotifyActionCallback func (*NotifyNotification, string, interface {})
 
 /*
  * Private Functions
@@ -149,7 +150,7 @@ func NotificationShow(notif *NotifyNotification) *GError {
 	return nil
 }
 
-func NotificationSetTimeout(notif *NotifyNotification, timeout int) {
+func NotificationSetTimeout(notif *NotifyNotification, timeout int32) {
 	C.notify_notification_set_timeout(notif._notification, C.gint(timeout))
 }
 
@@ -164,36 +165,132 @@ func NotificationSetUrgency(notif *NotifyNotification, urgency NotifyUrgency) {
 	C.notify_notification_set_urgency(notif._notification, C.NotifyUrgency(urgency))
 }
 
-// Member Functions
-func (this *NotifyNotification) Update(summary, body, icon string) bool {
-	psummary := C.CString(summary)
-	pbody := C.CString(body)
-	picon := C.CString(icon)
+// FIXME: implement
+func NotificationSetHint(notif *NotifyNotification, key string, value interface {}) {
+}
+
+func NotificationSetHintInt32(notif *NotifyNotification, key string, value int32) {
+	pkey := C.CString(key)
+	defer C.free(unsafe.Pointer(pkey))
+
+	C.notify_notification_set_hint_int32(notif._notification, pkey, C.gint(value))
+}
+
+func NotificationSetHintDouble(notif *NotifyNotification, key string, value float32) {
+	pkey := C.CString(key)
+	defer C.free(unsafe.Pointer(pkey))
+
+	C.notify_notification_set_hint_double(notif._notification, pkey, C.gdouble(value))
+}
+
+func NotificationSetHintString(notif *NotifyNotification, key string, value string) {
+	pkey := C.CString(key)
+	pvalue := C.CString(value)
 	defer func() {
-		C.free(unsafe.Pointer(psummary))
-		C.free(unsafe.Pointer(pbody))
-		C.free(unsafe.Pointer(picon))
+		C.free(unsafe.Pointer(pkey))
+		C.free(unsafe.Pointer(pvalue))
 	}()
 
-	return C.notify_notification_update(this._notification, psummary, pbody, picon) != 0
+	C.notify_notification_set_hint_string(notif._notification, pkey, pvalue)
+}
+
+func NotificationSetHintByte(notif *NotifyNotification, key string, value byte) {
+	pkey := C.CString(key)
+	defer C.free(unsafe.Pointer(pkey))
+
+	C.notify_notification_set_hint_byte(notif._notification, pkey, C.guchar(value))
+}
+
+// FIXME: implement
+func NotificationSetHintByteArray(notif *NotifyNotification, key string, value []byte, len uint32) {
+	pkey := C.CString(key)
+	defer C.free(unsafe.Pointer(pkey))
+
+	// C.notify_notification_set_hint_byte_array(notif._notification, pkey, (*C.guchar)(value), C.gsize(len))
+}
+
+func NotificationClearHints(notif *NotifyNotification) {
+	C.notify_notification_clear_hints(notif._notification)
+}
+
+// FIXME: the C function is supposed to be allowing the user to pass another function than free
+func NotificationAddAction(notif *NotifyNotification, action, label string, callback NotifyActionCallback, user_data interface {}) {
+	// C.notify_notification_add_action(notif._notification, paction, plabel, (C.NotifyActionCallback)(callback), user_data, C.free)
+}
+
+func NotificationClearActions(notif *NotifyNotification) {
+	C.notify_notification_clear_actions(notif._notification)
+}
+
+func NotificationClose(notif *NotifyNotification) *GError {
+	var err *C.GError
+
+	C.notify_notification_close(notif._notification, (**C.GError)(&err))
+
+	if err != nil {
+		return new_gerror(err)
+	}
+
+	return nil
+}
+
+// Member Functions
+func (this *NotifyNotification) Update(summary, body, icon string) bool {
+	return NotificationUpdate(this, summary, body, icon)
 }
 
 func (this *NotifyNotification) Show() {
-	var err *C.GError
-	C.notify_notification_show(this._notification, &err)
+	NotificationShow(this)
 }
 
-func (this *NotifyNotification) SetTimeout(timeout int) {
-	C.notify_notification_set_timeout(this._notification, C.gint(timeout))
+func (this *NotifyNotification) SetTimeout(timeout int32) {
+	NotificationSetTimeout(this, timeout)
 }
 
 func (this *NotifyNotification) SetCategory(category string) {
-	pcategory := C.CString(category)
-	defer C.free(unsafe.Pointer(pcategory))
-
-	C.notify_notification_set_category(this._notification, pcategory)
+	NotificationSetCategory(this, category)
 }
 
 func (this *NotifyNotification) SetUrgency(urgency NotifyUrgency) {
-	C.notify_notification_set_urgency(this._notification, C.NotifyUrgency(urgency))
+	NotificationSetUrgency(this, urgency)
+}
+
+func (this *NotifyNotification) SetHint(key string, value interface {}) {
+	NotificationSetHint(this, key, value)
+}
+
+func (this *NotifyNotification) SetHintInt32(key string, value int32) {
+	NotificationSetHintInt32(this, key, value)
+}
+
+func (this *NotifyNotification) SetHintDouble(key string, value float32) {
+	NotificationSetHintDouble(this, key, value)
+}
+
+func (this *NotifyNotification) SetHintString(key string, value string) {
+	NotificationSetHintString(this, key, value)
+}
+
+func (this *NotifyNotification) SetHintByte(key string, value byte) {
+	NotificationSetHintByte(this, key, value)
+}
+
+func (this *NotifyNotification) SetHintByteArray(key string, value []byte, len uint32) {
+	NotificationSetHintByteArray(this, key, value, len)
+}
+
+func (this *NotifyNotification) ClearHints() {
+	NotificationClearHints(this)
+}
+
+func (this *NotifyNotification) AddAction(action, label string, callback NotifyActionCallback, user_data interface {}) {
+	NotificationAddAction(this, action, label, callback, user_data)
+}
+
+func (this *NotifyNotification) ClearActions() {
+	NotificationClearActions(this)
+}
+
+func (this *NotifyNotification) Close() *GError {
+	return NotificationClose(this)
 }
